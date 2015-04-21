cc42c7e13cd718441a36e6f59d23975aec143e60
Lujun Weng <loyweng@163.com>
2015-04-21 21:39:38 +0800

    long time no see

---
+++ src/OSIM/OSIM.UnitTests/OSIM.Core/ItemRepositoryTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSIM.UnitTests.OSIM.Core
+{
+    public class when_working_with_the_item_type_repository : Specification
+    {
+    }
+
+    public class and_saving_a_valid_item_type : 
+        when_working_with_the_item_type_repository
+    {
+        [Test]
+        public void then_a_valid_item_type_id_should_be_returned()
+        {
+
+        }
+
+    }
+}
